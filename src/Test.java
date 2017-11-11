c5a798fc8ea2e843d964894f1e9ab762023003a6
Sean O'Hara <scohara@gmail.com>
2017-11-10 18:58:21 -0800

    Extracted Time and Date

--- src/Test.java
+++ src/Test.java
@@ -41,6 +41,83 @@ public class Test
         return temp;
     }
 
+    public static ArrayList<Integer> convertTimeToInt(String temp) {
+        ArrayList<Integer> classTimes = new ArrayList<>();
+        int time = 0;
+        int endIndex;
+        String day;
+        String time1;
+        String time2;
+        int time1p;
+        int time2p;
+
+        endIndex = temp.lastIndexOf(' ');
+        day = temp.substring(endIndex + 1);
+        temp = temp.substring(0, endIndex);
+
+        endIndex = temp.lastIndexOf('-');
+
+        if (day.equals("TBA")) {
+            classTimes.add(time);
+        } else {
+            time1 = temp.substring(0, endIndex);
+            time2 = temp.substring(endIndex + 1);
+            time1p = timeConversion(time1);
+            time2p = timeConversion(time2);
+            for (int i = 0; i < day.length(); i++) {
+                time = time2p;
+                time += time1p*10000;
+
+                switch (day.charAt(i)) {
+                    case 'M':
+                        break;
+                    case 'T':
+                        if (i + 1 < day.length() && day.charAt(i + 1) == 'h')
+                            time += 300000000;
+                        else
+                            time += 100000000;
+                        break;
+                    case 'W':
+                        time += 200000000;
+                        break;
+                    case 'F':
+                        time += 400000000;
+                        break;
+                    case 'S':
+                        time += 500000000;
+                        break;
+                    case 'U':
+                        time += 600000000;
+                        break;
+                    default:
+                        break;
+                }
+                classTimes.add(time);
+            }
+        }
+
+        return classTimes;
+    }
+
+    static int timeConversion(String s) {
+        String[] time = s.split(":");
+        String amPm = time[1].substring(3, 5);
+        int timeInt;
+
+        time[1] = time[1].substring(0, 2);
+
+        if (amPm.equals("AM") && time[0].equals("12"))
+            time[0] = "00";
+        else if (amPm.equals("PM") && Integer.parseInt(time[0]) < 12) {
+            timeInt = Integer.parseInt(time[0]);
+            timeInt = timeInt + 12;
+            time[0] = String.valueOf(timeInt);
+        }
+        String result = time[0] + time[1];
+
+        return Integer.parseInt(result);
+    }
+
     public static void main(String[] args) throws Exception
     {
         String url = "https://www.foothill.edu/anthropology/schedule.php";
@@ -71,7 +148,7 @@ public class Test
             String className;
             String instructor = "IDK";
             String type;
-            int time;
+            ArrayList<Integer> classTimes;
             String room;
 
             crn = Integer.parseInt(temp.substring(0,5));
@@ -101,12 +178,18 @@ public class Test
             room = temp.substring(endIndex + 1);
             temp = temp.substring(0, endIndex);
 
+            classTimes = convertTimeToInt(temp);
+
             System.out.println(temp);
             System.out.println("CRN: " + crn);
             System.out.println("Class Name: " + className);
             System.out.println("Instructor: " + instructor);
             System.out.println("Type: " + type);
-            System.out.println("Room: " + room + "\n");
+            System.out.println("Room: " + room);
+            for (int k = 0; k < classTimes.size(); k++) {
+                System.out.println("Time: " + classTimes.get(k));
+            }
+            System.out.println();
         }
 
 
