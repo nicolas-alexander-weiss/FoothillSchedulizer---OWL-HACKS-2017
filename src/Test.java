8b455acc24fc479813e8c66520a86e102d3e3499
Sean O'Hara <scohara@gmail.com>
2017-11-11 13:38:47 -0800

    Can retrieve ClassTimes, CRNs and Class Names from text

--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,9 +40,14 @@ public class Test
         return temp;
     }
 
-    public static ArrayList<Integer> convertTimeToInt(String temp) {
-        ArrayList<Integer> classTimes = new ArrayList<>();
+    public static ArrayList<ClassTime> createClassTime(ArrayList<String> timesToParse) {
+        ArrayList<ClassTime> classTimes = new ArrayList<>();
         int time = 0;
+        String type;
+        String instructor = "?";
+        String room;
+        String temp;
+
         int endIndex;
         String day;
         String time1;
@@ -51,48 +55,83 @@ public class Test
         int time1p;
         int time2p;
 
-        endIndex = temp.lastIndexOf(' ');
-        day = temp.substring(endIndex + 1);
-        temp = temp.substring(0, endIndex);
+        for (int i = 0; i < timesToParse.size(); i++) {
+            temp = timesToParse.get(i);
 
-        endIndex = temp.lastIndexOf('-');
+            endIndex = temp.indexOf('\t');
+            type = temp.substring(0, endIndex);
+            temp = temp.substring(endIndex + 1);
 
-        if (day.equals("TBA")) {
-            classTimes.add(time);
-        } else {
-            time1 = temp.substring(0, endIndex);
-            time2 = temp.substring(endIndex + 1);
-            time1p = timeConversion(time1);
-            time2p = timeConversion(time2);
-            for (int i = 0; i < day.length(); i++) {
-                time = time2p;
-                time += time1p*10000;
+            /*
+            String regex = "\\w*, [^\\s]+";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(temp);
+            if (matcher.find()) {
+                instructor = matcher.group();
+                temp = temp.substring(0,matcher.start() - 1) + temp.substring(matcher.end());
+            }
+            */
 
-                switch (day.charAt(i)) {
-                    case 'M':
-                        break;
-                    case 'T':
-                        if (i + 1 < day.length() && day.charAt(i + 1) == 'h')
-                            time += 300000000;
-                        else
-                            time += 100000000;
-                        break;
-                    case 'W':
-                        time += 200000000;
-                        break;
-                    case 'F':
-                        time += 400000000;
-                        break;
-                    case 'S':
-                        time += 500000000;
-                        break;
-                    case 'U':
-                        time += 600000000;
-                        break;
-                    default:
-                        break;
+            endIndex = temp.lastIndexOf('\t');
+            room = temp.substring(endIndex + 1);
+            temp = temp.substring(0, endIndex);
+
+            endIndex = temp.lastIndexOf('\t');
+            instructor = temp.substring(endIndex + 1);
+            temp = temp.substring(0, endIndex);
+
+
+
+            endIndex = temp.lastIndexOf('\t');
+            day = temp.substring(endIndex + 1);
+            temp = temp.substring(0, endIndex);
+
+            System.out.println(day);
+            System.out.println("Leftover: " + temp);
+
+            endIndex = temp.lastIndexOf('-');
+
+            if (room.equals("ONLINE")) {
+                classTimes.add(new ClassTime(0, room, type, instructor));
+            }
+            else {
+
+                time1 = temp.substring(0, endIndex);
+                time2 = temp.substring(endIndex + 1);
+                time1p = timeConversion(time1);
+                time2p = timeConversion(time2);
+                for (int j = 0; j < day.length(); j++) {
+                    time = time2p;
+                    time += time1p * 10000;
+
+                    switch (day.charAt(j)) {
+                        case 'M':
+                            break;
+                        case 'T':
+                            if (j + 1 < day.length() && day.charAt(j + 1) == 'h') {
+                                time += 300000000;
+                                j++;
+                            }
+                            else
+                                time += 100000000;
+                            break;
+                        case 'W':
+                            time += 200000000;
+                            break;
+                        case 'F':
+                            time += 400000000;
+                            break;
+                        case 'S':
+                            time += 500000000;
+                            break;
+                        case 'U':
+                            time += 600000000;
+                            break;
+                        default:
+                            break;
+                    }
+                    classTimes.add(new ClassTime(time, room, type, instructor));
                 }
-                classTimes.add(time);
             }
         }
 
@@ -120,13 +159,40 @@ public class Test
 
     public static void main(String[] args) throws Exception
     {
-        String url = "https://www.foothill.edu/anthropology/schedule.php";
-        Document document = Jsoup.connect(url).get();
 
-        String question = document.select(".listings").text();
+        String inputFileName = "resources/input.txt";
 
+        Scanner fileReader = new Scanner(new File(inputFileName));
+
+        String in = "";  //Stringbuilder!!
+
+        boolean stay = true;
+
+        while(stay){
+            try{
+                in += (" " + fileReader.nextLine());
+            }catch (Exception ex){
+                stay = false;
+            }
+        }
+
+        String question = in;
+        question = question.replace("\n", "").replace("\r", "");
+        System.out.println(question);
+
+        /*
         String outputFileName = "resources/output";
         BufferedWriter fileWriter;
+
+
+        try {
+            fileWriter = new BufferedWriter(new FileWriter(outputFileName + ".txt"));
+            fileWriter.write(question);
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
 
         String regex = "(\\d{5}.+?)Class";
 
@@ -151,13 +217,45 @@ public class Test
             ArrayList<Integer> classTimes;
             String room;
 
+
             crn = Integer.parseInt(temp.substring(0,5));
 
             temp = temp.substring(12);
 
             temp = removeGarbage(temp);
+
             className = temp.substring(0,9);
 
+
+            temp = temp.substring(13);
+
+            temp = removeGarbage(temp);
+
+            regex = "((?:L|T).+?(?:ONLINE|\\d{4}))";
+
+            pattern = Pattern.compile(regex);
+            matcher = pattern.matcher(temp);
+
+            ArrayList<String> timesToParse = new ArrayList<>();
+
+            String tempCopy = temp;
+
+            while (matcher.find()) {
+                String capture = matcher.group();
+                timesToParse.add(capture);
+                temp = temp.substring(0, matcher.start()) + temp.substring(matcher.end());
+                matcher = pattern.matcher(temp);
+            }
+
+
+
+            /*
+            int endIndex;
+            endIndex = temp.indexOf(' ');
+            type = temp.substring(0, endIndex);
+            temp = temp.substring(endIndex + 1);
+
+                        /*
             regex = "\\w*, [^\\s]+";
             pattern = Pattern.compile(regex);
             matcher = pattern.matcher(temp);
@@ -165,43 +263,43 @@ public class Test
                 instructor = matcher.group();
                 temp = temp.substring(0,matcher.start() - 1) + temp.substring(matcher.end());
             }
-            temp = temp.substring(13);
-
-            temp = startAtL(temp);
-
-            int endIndex;
-            endIndex = temp.indexOf(' ');
-            type = temp.substring(0, endIndex);
-            temp = temp.substring(endIndex + 1);
-
             endIndex = temp.lastIndexOf(' ');
             room = temp.substring(endIndex + 1);
             temp = temp.substring(0, endIndex);
 
-            classTimes = convertTimeToInt(temp);
+            //classTimes = convertTimeToInt(temp);*/
 
+            ArrayList<ClassTime> myClassTimes;
+            myClassTimes = createClassTime(timesToParse);
+
+            System.out.println(tempCopy);
             System.out.println(temp);
+            for (int mCounter = 0; mCounter < myClassTimes.size(); mCounter++) {
+                System.out.println(myClassTimes.get(mCounter));
+            }
+
+            for (int mCounter = 0; mCounter < timesToParse.size(); mCounter++) {
+                System.out.println(timesToParse.get(mCounter));
+            }
+
             System.out.println("CRN: " + crn);
             System.out.println("Class Name: " + className);
+
+
+
+                        /*
             System.out.println("Instructor: " + instructor);
             System.out.println("Type: " + type);
-            System.out.println("Room: " + room);
-            for (int k = 0; k < classTimes.size(); k++) {
+            System.out.println("Room: " + room);*/
+
+            /*for (int k = 0; k < classTimes.size(); k++) {
                 System.out.println("Time: " + classTimes.get(k));
-            }
+            }*/
             System.out.println();
         }
 
 
-        /*
-        try {
-            fileWriter = new BufferedWriter(new FileWriter(outputFileName + ".txt"));
-            fileWriter.write(wrapString);
-            fileWriter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        */
+
 
     }
 
