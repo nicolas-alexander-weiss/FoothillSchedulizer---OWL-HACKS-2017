2c362637dc26f355dcef819e73d1249b3bd69aed
weisscoding <30901564+weisscoding@users.noreply.github.com>
2017-11-11 10:57:50 -0800

    ClassName

---
+++ src/ClassName.java
@@ -0,0 +1,18 @@
+public class ClassName {
+    private String name;
+    private String department;
+
+    public ClassName(String className){
+
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
