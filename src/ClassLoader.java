2c1ae9a5daf448cc41c795e9b3eaaf19bf971408
weisscoding <30901564+weisscoding@users.noreply.github.com>
2017-11-11 10:37:23 -0800

    classLoader create

---
+++ src/ClassLoader.java
@@ -0,0 +1,16 @@
+public class ClassLoader {
+    private ClassLoader(){
+
+    }
+
+
+    /**
+     * Checks if the class the user entered exists
+     * @param depmt e.g. MATH or ANTH
+     * @param name e.g. 1A or 2B
+     * @return class file found
+     */
+    public static boolean classExists(String depmt, String name){
+
+    }
+}
